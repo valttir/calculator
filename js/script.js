a3f050aaf9dbe2f254ef8afcdbe07f33beef9dd9
Valtti Rinnemaa <99838812+valttir@users.noreply.github.com>
2023-11-10 16:12:42 +0200

    Add clicking number buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+let displayValue = "";
+let a;
+let operator;
+let b;
+
+const numbersDisplay = document.querySelector("#numbers");
+const clearButton = document.querySelector("#clear");
+const deleteButton = document.querySelector("#delete");
+const numButtons = document.querySelectorAll(".num");
+const opButtons = document.querySelectorAll(".op")
+
+
+for (let i = 0; i < numButtons.length; i++) {
+    numButtons[i].addEventListener("click", () => {
+        clickNumButton(numButtons[i].textContent)
+    })
+}
+
+
+
+function clickNumButton(num) {
+    if (displayValue.length <= 10) {
+        if (num === ".") {
+            if (!displayValue.includes(".")) {
+                displayValue += num;
+                numbersDisplay.textContent = displayValue;
+            }
+        } else {
+            displayValue += num;
+            numbersDisplay.textContent = displayValue;
+        }
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -13,10 +47,6 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
-
-let a;
-let operator;
-let b;
 
 function operate(a, operator, b) {
     switch (operator) {
@@ -34,3 +64,4 @@ function operate(a, operator, b) {
             break;
     }
 }
+
